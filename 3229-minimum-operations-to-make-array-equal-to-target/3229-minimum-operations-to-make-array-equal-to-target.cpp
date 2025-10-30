cec6d1dfbe6a6aed2d8fdd0e066af8d2064a1f30
Taranpreet Kaur <101043541+TARANPREETKAUR18@users.noreply.github.com>
2025-10-30 11:02:13 +0530

    Time: 0 ms (100%), Space: 186.1 MB (37.15%) - LeetHub

---
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long minimumOperations(vector<int>& nums, vector<int>& target) {
+        
+        int n = nums.size();
+
+        long long result = 0;
+
+        vector<int> diff(n);
+
+        for(int i = 0; i < n; i++){
+            diff[i] = target[i] - nums[i];
+        }
+
+        int curr = 0;
+        int prev = 0;
+
+        for(int i = 0; i < n; i++){
+            curr = diff[i];
+
+            //sign change
+            if((curr > 0 && prev < 0) || (curr < 0 && prev > 0)){
+                result += abs(curr); 
+            }
+          // both for no sign change
+            //collective increment/decrement for a subarray
+            else if(abs(curr) > abs(prev)){
+                result +=abs(curr - prev);
+            }
+            //separate increment/decrement
+            else if(abs(curr) <= abs(prev)){
+                //do nothing
+            }
+
+            prev = curr;
+        }
+
+        return result;
+    }
+};
