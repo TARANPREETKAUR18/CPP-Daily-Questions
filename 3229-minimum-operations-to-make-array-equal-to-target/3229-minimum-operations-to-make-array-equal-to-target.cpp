201242be9fcd151dc6c943e6640266ea6174b70e
Taranpreet Kaur <101043541+TARANPREETKAUR18@users.noreply.github.com>
2025-10-30 11:08:05 +0530

    Add complexity comments to minimum Operations function and Optimized version
    Added comments regarding time and space complexity.

--- 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
-        
+        // TC->O(2N)
+        // SC->O(N)
         int n = nums.size();
 
         long long result = 0;
@@ -38,3 +39,39 @@ public:
         return result;
     }
 };
+
+// class Solution {
+// public:
+//     long long minimumOperations(vector<int>& nums, vector<int>& target) {
+        
+//         int n = nums.size();
+
+//         long long result = 0;
+
+//         int curr = 0;
+//         int prev = 0;
+//         // TC->O(N)
+//         // SC->O(1)
+//         for(int i = 0; i < n; i++){
+//             curr = target[i] - nums[i];
+
+//             //sign change
+//             if((curr > 0 && prev < 0) || (curr < 0 && prev > 0)){
+//                 result += abs(curr); 
+//             }
+//           // both for no sign change
+//             //collective increment/decrement for a subarray
+//             else if(abs(curr) > abs(prev)){
+//                 result +=abs(curr - prev);
+//             }
+//             //separate increment/decrement
+//             else if(abs(curr) <= abs(prev)){
+//                 //do nothing
+//             }
+
+//             prev = curr;
+//         }
+
+//         return result;
+//     }
+// };
