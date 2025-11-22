4ee7cb3d8f5cd50ab1e8fdaa4c173fdde5daba1a
Taranpreet Kaur <101043541+TARANPREETKAUR18@users.noreply.github.com>
2025-11-22 14:54:31 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.83%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -7,14 +7,10 @@ public:
         unordered_map<int, int> mp;
 
         for(int &num: nums){
+            result += mp[num];
             mp[num]++;
         }
 
-        for(auto &it: mp){
-            int count = it.second;
-            result += count*(count-1)/2;
-        }
-        
         return result;
     }
 };
