32b8b30c8c53b438c373f3bfeaccbe3c8baccce1
Taranpreet Kaur <101043541+TARANPREETKAUR18@users.noreply.github.com>
2026-09-26 23:49:19 +0530

    Time: 83 ms (52.02%), Space: 122.8 MB (46.73%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,34 +2,29 @@ class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         int n = s.length();
+
         unordered_map<string, string> mp;
 
-
         for(auto &vec : knowledge) {
+            //vec[0], vec[1]
             mp[vec[0]] = vec[1];
         }
 
         string result = "";
-        string temp = "";
-        bool bracketOpened = false;
         int i = 0;
-        
+
         while(i < n) {
             if(s[i] == '(') {
-                bracketOpened = true;
-            } else if(s[i] == ')') {
+                int j = s.find(")", i+1);
+                string temp = s.substr(i+1, j-i-1);
                 result += mp.count(temp) ? mp[temp] : "?";
-                bracketOpened = false;
-                temp = "";
-            } else if(bracketOpened) {
-                temp.push_back(s[i]);
+                i = j;
             } else {
                 result.push_back(s[i]);
             }
 
             i++;
         }
-
         return result;
     }
 };
